c074e478887450cc76134384376e6f3c3d14ae97
iku0n <ku0n1355@protonmail.com>
2020-09-14 20:51:16 +0800

    Fiverr: Check Process

---
+++ check_process.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+
+
+function check_mysql {
+    echo -e "[INFO] Checking mysql process if running....."
+        if pgrep mysqld > /dev/null 2>&1; then
+            echo "[INFO] mysqld is running....."
+        else
+            echo "[INFO] mysqld is stopped....."
+            echo "[INFO] starting mysqld service"
+                systemctl start mariadb
+                sleep 3
+            echo "[INFO] mysqld is started....."
+}
+
+function check_httpd {
+    echo -e "[INFO] Checking httpd process if running....."
+        if pgrep httpd > /dev/null 2>&1; then
+            echo "[INFO] httpd is running....."
+        else
+            echo "[INFO] httpd is stopped....."
+            echo "[INFO] starting httpd service"
+                systemctl start httpd
+                sleep 3
+            echo "[INFO] httpd is started....."
+}
+
+function check_process {
+    check_mysql
+    check_httpd
+}
+
+check_process
